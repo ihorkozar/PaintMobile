9d50da2756d07fcc4d7ae14dcb92cbbdacb8baf9
Ihor_Kozar <ihor_kozar@epam.com>
2021-08-12 13:58:24 +0300

    v 1.0

--- app/src/main/java/com/example/paintmobile/CustomCanvasView.kt
+++ app/src/main/java/com/example/paintmobile/CustomCanvasView.kt
@@ -1,11 +1,17 @@
 package com.example.paintmobile
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.Path
+import android.graphics.Rect
+import android.view.MotionEvent
 import android.view.View
+import android.view.ViewConfiguration
 import androidx.core.content.res.ResourcesCompat
+import kotlin.math.abs
 
 private const val STROKE_WIDTH = 12f
 
@@ -23,6 +29,13 @@ class CustomCanvasView (context: Context) : View(context) {
         strokeCap = Paint.Cap.ROUND
         strokeWidth = STROKE_WIDTH
     }
+    private var path = Path()
+    private var motionTouchEventX = 0f
+    private var motionTouchEventY = 0f
+    private var currentX = 0f
+    private var currentY = 0f
+    private val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop
+    private lateinit var frame: Rect
 
     override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
         super.onSizeChanged(width, height, oldWidth, oldHeight)
@@ -30,10 +43,48 @@ class CustomCanvasView (context: Context) : View(context) {
         extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
         extraCanvas = Canvas(extraBitmap)
         extraCanvas.drawColor(backgroundColor)
+        val inset = 40
+        frame = Rect(inset, inset, width - inset, height - inset)
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         canvas?.drawBitmap(extraBitmap, 0f, 0f, null)
+        canvas?.drawRect(frame, paint)
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        motionTouchEventX = event.x
+        motionTouchEventY = event.y
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> touchStart()
+            MotionEvent.ACTION_MOVE -> touchMove()
+            MotionEvent.ACTION_UP -> touchUp()
+        }
+        return true
+    }
+
+    private fun touchStart() {
+        path.reset()
+        path.moveTo(motionTouchEventX, motionTouchEventY)
+        currentX = motionTouchEventX
+        currentY = motionTouchEventY
+    }
+
+    private fun touchMove() {
+        val dx = abs(motionTouchEventX - currentX)
+        val dy = abs(motionTouchEventY - currentY)
+        if (dx >= touchTolerance || dy >= touchTolerance) {
+            path.quadTo(currentX, currentY, (motionTouchEventX + currentX) / 2, (motionTouchEventY + currentY) / 2)
+            currentX = motionTouchEventX
+            currentY = motionTouchEventY
+            extraCanvas.drawPath(path, paint)
+        }
+        invalidate()
+    }
+
+    private fun touchUp() {
+        path.reset()
     }
 }
